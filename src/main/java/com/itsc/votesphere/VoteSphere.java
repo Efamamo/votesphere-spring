e893ce133b27ff4c5b17d16a7ddce095a33e1416
ephrem <ephremmamo555@gmail.com>
2024-12-24 16:27:41 +0300

    add: add login and signup pages

--- src/main/java/com/itsc/votesphere/VoteSphere.java
+++ src/main/java/com/itsc/votesphere/VoteSphere.java
@@ -1,6 +1,5 @@
 package com.itsc.votesphere;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/com/itsc/votesphere/auth/AuthController.java
+++ src/main/java/com/itsc/votesphere/auth/AuthController.java
@@ -1,59 +1,68 @@
 package com.itsc.votesphere.auth;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.itsc.votesphere.users.User;
 import com.itsc.votesphere.users.UserService;
-
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/auth")
 public class AuthController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/signup")
-    public ResponseEntity<String> signup(@RequestBody @Valid User user){
-        Boolean emailTaken = userService.emailTaken(user.getEmail());
+    @PostMapping("/auth/signup")
+    public ResponseEntity<Map<String, String>>signup(@RequestBody @Valid User user){
+        Map<String, String> response = new HashMap<>();
 
+        Boolean emailTaken = userService.emailTaken(user.getEmail());
         if (emailTaken){
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Email is Taken");
+            response.put("email", "Email is Taken");
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
         }
 
         Boolean usernameTaken = userService.usernameTaken(user.getUsername());
-
         if (usernameTaken){
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Username is Taken");
-
+            response.put("username", "Username is Taken");
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
         }
 
         userService.addUser(user);
-        return ResponseEntity.status(HttpStatus.CREATED).body("User Created Successfully");
+        response.put("success", "Signedup Successfully");
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody @Valid Login loginData){
+
+    @PostMapping("/auth/login")
+    public ResponseEntity<Map<String, String>> login(@RequestBody @Valid Login loginData){
+        Map<String, String> response = new HashMap<>();
         User user = userService.findUserByUsername(loginData.getUsername());
         if (user == null){
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized");
+            response.put("error", "Unauthorized");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
         }
 
         Boolean passwordMatch = userService.checkPassword(loginData.getPassword(), user.getPassword());
         if (!passwordMatch){
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized");
-        }
+            response.put("error", "Unauthorized");
 
-        return ResponseEntity.status(HttpStatus.ACCEPTED).body("Logged In Successfu");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+        }
+        response.put("success", "Logged In Successfully");
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
 
 
     }
 
+   
 
     
 }
