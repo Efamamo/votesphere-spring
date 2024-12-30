3febe15cc06fa28233c86c7a0698723df8842792
ephrem <ephremmamo555@gmail.com>
2024-12-30 10:51:58 +0300

    add: add single poll fetch and reactions ui

--- src/main/java/com/itsc/votesphere/polls/PollsPageController.java
+++ src/main/java/com/itsc/votesphere/polls/PollsPageController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.itsc.votesphere.users.UserService;
@@ -121,6 +122,92 @@ public class PollsPageController {
 
 
     return "polls";
+
+    }
+
+
+    @GetMapping("/{id}")
+    public String getPollById(Model model, @PathVariable Long id, HttpServletRequest request, HttpServletResponse response) throws IOException, java.io.IOException{
+        Claims claims = (Claims) request.getAttribute("user");
+        String username = claims.getSubject();  
+
+        User user = userService.findUserByUsername(username);
+
+        if (user == null){
+            response.sendRedirect("/auth/login");  
+            return null;
+        }
+
+        Group userGroup = user.getMemberOf();
+
+        if (userGroup == null){
+            response.sendRedirect("/polls");  
+            return null;
+
+        }
+
+
+        Poll poll = pollRepository.findById(id).orElse(null);
+
+        if (poll == null){
+            response.sendRedirect("/polls");  
+            return null;
+        }
+
+        Map<String, Object> pollData = new HashMap<>();
+            pollData.put("id", poll.getId());
+            pollData.put("question", poll.getQuestion());
+    
+            // Calculate the total votes for the poll
+            final int total = poll.getChoices().stream()
+                .mapToInt(choice -> choice.getVotes().size())
+                .sum();
+           
+    
+            // Map the choices
+            List<Map<String, String>> choiceContents = poll.getChoices().stream()
+                .map(choice -> {
+                     Choice userVote = null;
+
+            
+
+
+            for (Vote vote: user.getVotes()){
+                if (vote.getPoll() == poll){
+                    userVote = vote.getChoice();
+                    break;
+                }
+            }
+
+
+            
+                    Map<String, String> choiceData = new HashMap<>();
+                    choiceData.put("content", choice.getContent());
+                    choiceData.put("checked", choice == userVote ? "yes": "no");
+
+
+                    if (total == 0) {
+                        choiceData.put("count", "0");
+                    } else {
+                        choiceData.put("count", String.valueOf((choice.getVotes().size()) / (double) total * 100));
+                    }                     choiceData.put("id", choice.getId().toString());
+                    return choiceData;
+                })
+                .collect(Collectors.toList());
+            
+           
+
+
+                pollData.put("choices", choiceContents);
+                pollData.put("total", total == 0 ? 1 : total);
+
+                model.addAttribute("isAdmin", user.getIsAdmin());
+                model.addAttribute("hasGroup", true);
+
+                model.addAttribute("poll", pollData);
+
+                return "poll";
+
 
     }
 
